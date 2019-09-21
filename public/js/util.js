1407cf5045d59f2a250c9c4252f979925e77c3b8
Ali Rehman <rehmanaa@uci.edu>
2019-09-21 09:23:06 -0700

    Select only show last names, edit match doubles only last names and clear button

--- public/js/util.js
+++ public/js/util.js
@@ -18,6 +18,7 @@ function formatPlayerNameDraw(name) {
     var doubles1 = "";
     var doubles2 = "";
     var doubles = false;
+    var lastName = getlastName(parts[0]);
     if (parts[0].includes("/")) {
         var doublesSplit = parts[0].split("/");
         doubles1 = getlastName(doublesSplit[0]);
@@ -30,7 +31,7 @@ function formatPlayerNameDraw(name) {
         if (doubles) {
             result = doubles1 + "/" + doubles2 + ", " + parts[1];
         } else {
-            result = parts[0] + ", " + parts[1];
+            result = lastName + ", " + parts[1];
         }
     }
     else if (parts.length != 3) {
@@ -40,22 +41,22 @@ function formatPlayerNameDraw(name) {
         if (doubles) {
             result = doubles1 + "/" + doubles2 + ", " + parts[1];
         } else {
-            result = parts[0] + ", " + parts[1];
+            result = lastName + ", " + parts[1];
         }
         result += " (" + parts[2] + ")";
     }
     if (result.length >= 26 && !doubles) {
-        var lastSpace = 0;
+        /*var lastSpace = 0;
         for (var i = 0; i < parts[0].length; i++) {
             if (parts[0][i] == " ") {
                 lastSpace = i;
             }
         }
-        result = result.substring(lastSpace);
+        result = result.substring(lastSpace);*/
 
-        var maxTeamName = 21 - parts[0].substring(lastSpace).length;
+        var maxTeamName = 21 - lastName.length;
         if (parts[1].length > maxTeamName) {
-            result = parts[0].substring(lastSpace) + ", " + parts[1].substring(0,maxTeamName-3) + "...";
+            result = lastName + ", " + parts[1].substring(0,maxTeamName-3) + "...";
             if (parts.length == 3) {
                 result += " (" + parts[2] + ")";
             }
@@ -88,13 +89,31 @@ function formatPlayerNameDrawEdit(name) {
         return "";
     }
     var parts = name.split(",");
+    var doubles1 = "";
+    var doubles2 = "";
+    var doubles = false;
+    if (parts[0].includes("/")) {
+        var doublesSplit = parts[0].split("/");
+        doubles1 = getlastName(doublesSplit[0]);
+        doubles2 = getlastName(doublesSplit[1]);
+        doubles = true;
+    }
     if (parts.length == 2) {
-        return parts[0] + ", " + parts[1];
+        if (doubles) {
+            return doubles1 + "/" + doubles2 + ", " + parts[1];
+        } else {
+            return parts[0] + ", " + parts[1];
+        }
     }
     if (parts.length != 3) {
         return name;
     }
-    var result = parts[0] + ", " + parts[1];
+    var result = "";
+    if (doubles) {
+        result = doubles1 + "/" + doubles2 + ", " + parts[1];
+    } else {
+        result = parts[0] + ", " + parts[1];
+    }
     if (parts.length == 3) {
         result += " (" + parts[2] + ")";
     }
